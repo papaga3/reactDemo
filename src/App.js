db06705dbbb43df375021ab95c51699d74be87a8
Quoc Huy Nguyen <quochuynguyen@Quocs-MacBook-Pro.local>
2024-01-18 10:40:48 +0200

    small update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          new application
         </a>
       </header>
     </div>
